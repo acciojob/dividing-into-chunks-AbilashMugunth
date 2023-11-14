6166e3fcf7f89855050a74b0f43c0db0295c43eb
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-14 19:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-const divide = (arr, n) => {
-  // Write your code here
+const divide = (arr, max) => {
+  let currentSum=0;
+   let subArray=[];
+	let result=[];
+
+	for(const num of arr){
+		if(currentSum+num<=max){
+			subArray.push(num);
+			currentSum+=num;
+		}else{
+			result.push(subArray);
+			subArray=[num];
+			currentSum=num;
+		}
+	}
+
+	result.push(subArray);
+	return result;
 };
 
 const n = prompt("Enter n: ");
